7856d5aba8730b2f5b951a01b63d1e1ba5117498
Gaston Recalde <gastonrg94@gmail.com>
2022-04-18 11:26:08 -0300

    Formulario con mensaje de error implementado

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Barra de navegación responsive
 const navToggle = document.querySelector(".nav__toggle");
 const navUl = document.querySelector(".nav__ul");
 const body = document.querySelector("body");
@@ -16,3 +17,100 @@ navToggle.addEventListener("click", (e) => {
 body.addEventListener("click", () => {
     navUl.classList.remove("nav__ul__visible");
 });
+
+// Fin de barra de navegación
+
+// Control de el Formulario
+const form = document.getElementById('form');
+
+// form.addEventListener('submit', handleSubmit)
+
+// async function handleSubmit(event){
+//   event.preventDefault()
+
+//   const form = new FormData(this)
+//   const response = await fetch(this.action, {
+//     method: this.method,
+//     body: form,
+//     headers: {
+//       'Accept': 'application/json'
+//     }
+//   })
+//   if (response.ok) {
+//     this.reset();
+//     showAlert()
+//   }
+// }
+
+// Alerta al enviar formulario
+// const button = document.querySelector("#button");
+// button.addEventListener("click", showAlert);
+
+// function showAlert() {
+//     Swal.fire({
+//         position: 'center',
+//         icon: 'success',
+//         title: 'Mensaje enviado',
+//         showConfirmButton: false,
+//         timer: 1500
+//       })
+// }
+
+
+
+
+
+
+// Validacion del formulario
+const usuario = document.getElementById('username');
+const email = document.getElementById('email');
+const message = document.getElementById('message');
+
+form.addEventListener('submit', e => {
+	e.preventDefault();
+	
+	checkInputs();
+});
+
+function checkInputs() {
+	// trim to remove the whitespaces
+	const usuarioValue = usuario.value.trim();
+	const emailValue = email.value.trim();
+  const messageValue = message.value.trim();
+	
+	if(usuarioValue === '') {
+		setErrorFor(usuario, 'No puede dejar el nombre en blanco');
+	} else {
+		setSuccessFor(usuario);
+	}
+	
+	if(emailValue === '') {
+		setErrorFor(email, 'No puede dejar el email en blanco');
+	} else if (!isEmail(emailValue)) {
+		setErrorFor(email, 'No ingreso un email válido');
+	} else {
+		setSuccessFor(email);
+	}
+
+  if(messageValue === '') {
+		setErrorFor(message, 'No puede dejar el mensaje en blanco');
+	} else {
+		setSuccessFor(message);
+	}
+}
+
+function setErrorFor(input, message) {
+	const formControl = input.parentElement;
+	const small = formControl.querySelector('small');
+	formControl.className = 'form-control error';
+	small.innerText = message;
+}
+
+function setSuccessFor(input) {
+	const formControl = input.parentElement;
+	formControl.className = 'form-control success';
+}
+
+function isEmail(email) {
+	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}

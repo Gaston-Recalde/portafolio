ef783468b7eae64f99877489e552ea3960d5ce47
Gaston Recalde <gastonrg94@gmail.com>
2022-04-19 09:54:59 -0300

    Alertas del Formulario implementadas

--- app.js
+++ app.js
@@ -22,46 +22,6 @@ body.addEventListener("click", () => {
 
 // Control de el Formulario
 const form = document.getElementById('form');
-
-// form.addEventListener('submit', handleSubmit)
-
-// async function handleSubmit(event){
-//   event.preventDefault()
-
-//   const form = new FormData(this)
-//   const response = await fetch(this.action, {
-//     method: this.method,
-//     body: form,
-//     headers: {
-//       'Accept': 'application/json'
-//     }
-//   })
-//   if (response.ok) {
-//     this.reset();
-//     showAlert()
-//   }
-// }
-
-// Alerta al enviar formulario
-// const button = document.querySelector("#button");
-// button.addEventListener("click", showAlert);
-
-// function showAlert() {
-//     Swal.fire({
-//         position: 'center',
-//         icon: 'success',
-//         title: 'Mensaje enviado',
-//         showConfirmButton: false,
-//         timer: 1500
-//       })
-// }
-
-
-
-
-
-
-// Validacion del formulario
 const usuario = document.getElementById('username');
 const email = document.getElementById('email');
 const message = document.getElementById('message');
@@ -97,6 +57,7 @@ function checkInputs() {
 	} else {
 		setSuccessFor(message);
 	}
+
 }
 
 function setErrorFor(input, message) {
@@ -113,4 +74,49 @@ function setSuccessFor(input) {
 
 function isEmail(email) {
 	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
+
+// Alertas de Formulario
+// const button = document.querySelector("#button");
+// button.addEventListener("click", showAlert, showAlert2);
+
+const showAlert = () => {
+    Swal.fire({
+        position: 'center',
+        icon: 'success',
+        title: 'Mensaje enviado',
+        showConfirmButton: false,
+        timer: 1500
+      })
+}
+
+const showAlert2 = () => {
+  Swal.fire({
+    position: 'center',
+    icon: 'error',
+    title: 'Error, mensaje no enviado',
+    showConfirmButton: false,
+    timer: 1500
+  })
+}
+
+form.addEventListener('submit', handleSubmit)
+
+async function handleSubmit(event){
+  event.preventDefault();
+
+  const form = new FormData(this)
+  const response = await fetch(this.action, {
+    method: this.method,
+    body: form,
+    headers: {
+      'Accept': 'application/json'
+    }
+  })
+  if (response.ok) {
+    this.reset();
+    showAlert();
+  } else {
+    showAlert2();
+  }
 }
